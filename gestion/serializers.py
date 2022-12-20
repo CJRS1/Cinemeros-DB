e5d61c2eb73cc713960328183dd116ebc655d4c6
CJRS1 <reyes.christian@pucp.pe>
2022-12-19 20:24:54 -0800

    Se corrigó el archivo requirements.txt

--- gestion/serializers.py
+++ gestion/serializers.py
@@ -5,14 +5,11 @@ from rest_framework import serializers
 class UsuarioSerializer(serializers.ModelSerializer):
 
     def save(self):
-
         nuevoUsuario = UsuarioModel(**self.validated_data)
-
         nuevoUsuario.set_password(self.validated_data.get('password'))
-
         nuevoUsuario.save()
-
         return nuevoUsuario
+        
     class Meta:
         fields = '__all__'
         model = UsuarioModel
