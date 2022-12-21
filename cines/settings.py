0c29e15c84fe95518fa79724fd70f71ed7eab014
CJRS1 <reyes.christian@pucp.pe>
2022-12-20 20:28:50 -0800

    Se modifico DATABASES

--- cines/settings.py
+++ cines/settings.py
@@ -85,17 +85,23 @@ WSGI_APPLICATION = 'cines.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': environ.get('DB_NAME'),
-        'USER': environ.get('DB_USER'),
-        'PASSWORD': environ.get('DB_PASSWORD'),
-        'HOST': environ.get('DB_HOST'),
-        'PORT': environ.get('DB_PORT')
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': environ.get('DB_NAME'),
+#         'USER': environ.get('DB_USER'),
+#         'PASSWORD': environ.get('DB_PASSWORD'),
+#         'HOST': environ.get('DB_HOST'),
+#         'PORT': environ.get('DB_PORT')
+#     }
+# }
 
+DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+}
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
