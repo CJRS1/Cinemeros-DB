3cfd6f85987b31053ba76cfd119fd3c464f90cb6
CJRS1 <reyes.christian@pucp.pe>
2022-12-25 22:11:24 -0800

    Agregué Allowed HOSTS

--- cines/settings.py
+++ cines/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-# ALLOWED_HOSTS = ['cinemerosdb.herokuapp.com']
+ALLOWED_HOSTS = ['cinemerosdb.herokuapp.com','localhost','127.0.0.1']
 
 
 # Application definition
